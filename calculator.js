2edb5de4f3afa28ddafd958c5fc024c6a2daf408
Gabriele <gabrimassi@rocketmail.com>
2023-01-23 23:22:22 +0100

    Added and implemented new keys.

--- calculator.js
+++ calculator.js
@@ -3,18 +3,57 @@ operator.forEach((button)=>{
     button.addEventListener('click', takeInput);
 });
 
+const numpad=document.querySelectorAll(".numbers");
+numpad.forEach((button)=>{
+    button.addEventListener('click', displayNumber);
+})
+
+const displayField=document.getElementById("display");
+
 const evaluator=document.querySelector("#operate");
 evaluator.addEventListener('click', evaluate);
 
-let math=[2], action='', result;
-const inputField=document.getElementById("numInput");
-inputField.value='';
+const resetButton=document.getElementById("reset");
+resetButton.addEventListener('click', ()=>{
+    displayField.textContent='';
+    math=[];
+});
+
+const clearDisplay=document.getElementById("clearDisp");
+clearDisplay.addEventListener('click', ()=>{
+    displayField.textContent='';
+});
+
+const backspace=document.getElementById("backspace");
+backspace.addEventListener('click', charDelete);
+
+function charDelete(){
+    oldDisplay=displayField.textContent;
+    //console.log("oldDisplay: "+oldDisplay);
+        if(oldDisplay.length>1) displayField.textContent=oldDisplay.slice(0, oldDisplay.length-1);
+        else if(oldDisplay.length===1) displayField.textContent=''
+}
+
+let math=[3];
+
+let oldDisplay='';
+/*const inputField=document.getElementById("numInput");
+inputField.value='';*/
+
+function displayNumber(e){
+    displayField.textContent+=e.target.id;
+}
 
 function takeInput(e){
-    math[0]=parseInt(inputField.value, 10);
+    /*math[0]=parseInt(inputField.value, 10);
     inputField.value='';
-    console.log("takeinput id: "+e.target.id);
-    switch(e.target.id){
+    console.log("takeinput id: "+e.target.id);*/
+    math[0]=parseInt(displayField.textContent, 10);
+    displayField.textContent='';
+    math[1]=e.target.id;
+    /*console.log("displaying math array...");
+    console.log(math);*/
+    /*switch(e.target.id){
         case 'add':
             action='a';
             break;
@@ -27,29 +66,33 @@ function takeInput(e){
         case 'divide':
             action='d';
             break;
-    }
+    }*/
 }
 
 function evaluate(){
-    math[1]=parseInt(inputField.value, 10);
-    inputField.value='';
-    switch(action){
-        case 'a':
-            add();
+    math[2]=parseInt(displayField.textContent, 10);
+    //inputField.value='';
+    switch(math[1]){
+        case 'add':
+            displayField.textContent=math[0]+math[2];
             break;
-        case 's':
-            subtract();
+        case 'subtract':
+            displayField.textContent=math[0]-math[2];
             break;
-        case 'm':
-            multiply();
+        case 'multiply':
+            displayField.textContent=math[0]*math[2];
             break;
-        case 'd':
-            divide();
+        case 'divide':
+            if(math[2]===0){
+                displayField.textContent="Can't divide by 0!";
+                break;
+            }
+            displayField.textContent=math[0]/math[2];
             break;
     }
 }
 
-function add(){
+/*function add(){
     result=math[0] + math[1];
     inputField.value=result;
 }
