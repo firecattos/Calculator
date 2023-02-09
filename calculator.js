ddaeda4c8d8ba4d1ca81ebe62545e0105b210b20
Gabriele <gabrimassi@rocketmail.com>
2023-02-09 22:57:55 +0100

    Changed the working logic of the calculator and fixed a few flaws.

--- calculator.js
+++ calculator.js
@@ -6,18 +6,15 @@ operator.forEach((button)=>{
 const numpad=document.querySelectorAll(".numbers");
 numpad.forEach((button)=>{
     button.addEventListener('click', displayNumber);
-})
+});
 
 const displayField=document.getElementById("display");
-
+/*
 const evaluator=document.querySelector("#operate");
 evaluator.addEventListener('click', evaluate);
-
+*/
 const resetButton=document.getElementById("reset");
-resetButton.addEventListener('click', ()=>{
-    displayField.textContent='';
-    math=[];
-});
+resetButton.addEventListener('click', resetCalc);
 
 const clearDisplay=document.getElementById("clearDisp");
 clearDisplay.addEventListener('click', ()=>{
@@ -25,111 +22,109 @@ clearDisplay.addEventListener('click', ()=>{
 });
 
 const backspace=document.getElementById("backspace");
-backspace.addEventListener('click', charDelete);
+backspace.addEventListener('click', bksp);
 
-function charDelete(){
+let math=[], oldDisplay='';
+let resultCheck=0, terminator=0, operatorCheck=0;
+
+function bksp(){    //Backspace
     oldDisplay=displayField.textContent;
-    //console.log("oldDisplay: "+oldDisplay);
-        if(oldDisplay.length>1) displayField.textContent=oldDisplay.slice(0, oldDisplay.length-1);
-        else if(oldDisplay.length===1) displayField.textContent=''
+    if(oldDisplay.charAt(0)==='C') displayField.textContent='';
+    else if(oldDisplay.length>1) displayField.textContent=oldDisplay.slice(0, oldDisplay.length-1);
+    else if(oldDisplay.length===1) displayField.textContent=''
 }
 
-let math=[3];
+function displayNumber(e){  //Displays the number that is clicked
+    if(terminator===1){
+        console.log("resetfunc call");
+        resetCalc();
+        terminator=0;
+    }
 
-let oldDisplay='';
-/*const inputField=document.getElementById("numInput");
-inputField.value='';*/
+    if(resultCheck===1){
+        displayField.textContent='';
+        resultCheck=0;
+    }
+    if(operatorCheck===1) operatorCheck=0;
 
-function displayNumber(e){
-    displayField.textContent+=e.target.id;
+    if(e.target.id==="period") displayField.textContent+=".";
+    else displayField.textContent+=e.target.id;
 }
 
-function takeInput(e){
-    /*math[0]=parseInt(inputField.value, 10);
-    inputField.value='';
-    console.log("takeinput id: "+e.target.id);*/
-    math[0]=parseInt(displayField.textContent, 10);
+function takeInput(e){  //When an operator is clicked, it takes the input and the operator
+    if(terminator===1){     //Allows user to continue operating with the result, cleaning all the rest
+        let swapDisp=displayField.textContent;
+        console.log("resetfunc call");
+        resetCalc();
+        terminator=0;
+        math.push(swapDisp);
+        math.push(e.target.id);
+        displayField.textContent='';
+        return;
+    }
+
+    if(math.length===0){    //First operand check
+        math.push(displayField.textContent);
+        math.push(e.target.id);
+        operatorCheck=1;
+        displayField.textContent='';
+        return;
+    }
+
+    if(operatorCheck===1){  //Check for double operator insertion
+        console.log("test double operator check");
+        math[math.length-1]=e.target.id;
+        return;
+    }
+
+    math.push(displayField.textContent);
+    math.push(e.target.id);
+    operatorCheck=1;
     displayField.textContent='';
-    math[1]=e.target.id;
-    /*console.log("displaying math array...");
-    console.log(math);*/
-    /*switch(e.target.id){
-        case 'add':
-            action='a';
-            break;
-        case 'subtract':
-            action='s';
-            break;
-        case 'multiply':
-            action='m';
-            break;
-        case 'divide':
-            action='d';
-            break;
-    }*/
-}
 
-function evaluate(){
-    math[2]=parseInt(displayField.textContent, 10);
-    //inputField.value='';
-    switch(math[1]){
-        case 'add':
-            displayField.textContent=math[0]+math[2];
-            break;
-        case 'subtract':
-            displayField.textContent=math[0]-math[2];
-            break;
-        case 'multiply':
-            displayField.textContent=math[0]*math[2];
-            break;
-        case 'divide':
-            if(math[2]===0){
-                displayField.textContent="Can't divide by 0!";
+    //fix floating point operations
+
+    let switchId=math.length;
+    //console.log("switchId: "+switchId);
+    if(switchId>2){
+    switch(math[switchId-3]){
+            case 'add':
+                displayField.textContent=parseInt(math[switchId-4])+parseInt(math[switchId-2]);
+                math[switchId-2]=displayField.textContent;
                 break;
-            }
-            displayField.textContent=math[0]/math[2];
-            break;
+            case 'subtract':
+                displayField.textContent=parseInt(math[switchId-4])-parseInt(math[switchId-2]);
+                math[switchId-2]=displayField.textContent;
+                break;
+            case 'multiply':
+                displayField.textContent=parseInt(math[switchId-4])*parseInt(math[switchId-2]);
+                math[switchId-2]=displayField.textContent;
+                break;
+            case 'divide':
+                if(math[switchId-2]===0){
+                    displayField.textContent="Can't divide by 0!";
+                    terminator=1;
+                    break;
+                }
+                displayField.textContent=parseInt(math[switchId-4])/parseInt(math[switchId-2]);
+                math[switchId-2]=displayField.textContent;
+                break;
+           case 'percentage':
+
+                alert("work in progress...");
+                break;
+        }
+    resultCheck=1;
     }
+    
+    if(math[switchId-1]==='operate')
+        terminator=1;
 }
 
-/*function add(){
-    result=math[0] + math[1];
-    inputField.value=result;
+function resetCalc(){   //Completely resets the calculator
+    console.log("Reset function");
+    
+    displayField.textContent='';
+    math=[];
+    return;
 }
-
-function subtract(){
-    result=math[0] - math[1];
-    inputField.value=result;
-}
-
-function multiply(){
-    result=math[0] * math[1];
-    inputField.value=result;
-}
-
-function divide(){
-    result=math[0] / math[1];
-    inputField.value=result;
-}
-
-/*let num1, num2;
-
-function calculate(e){
-    num1=document.getElementById("numInput").value;
-    document.getElementById("numInput").value='';
-    switch(e.target.id){
-        case add:
-            add(num1);
-            break;
-        case subtract:
-            subtract(num1);
-            break;
-        case multiply:
-            multiply(num1);
-            break;
-        case divide:
-            divide(num1);
-            break;
-    }
-}
-*/
