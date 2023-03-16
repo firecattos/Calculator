ad9b30f90b38f2f88c996bc18491c6b96b9843d3
Gabriele <gabrimassi@rocketmail.com>
2023-03-16 23:11:46 +0100

    Working dual line display.

--- calculator.js
+++ calculator.js
@@ -12,19 +12,20 @@ numpad.forEach((button)=>{
     });
 });
 
-//const displayField=document.getElementById("display");
-const displayField=document.getElementById("lowerRow");
+//const lowerDisp=document.getElementById("display");
+//const lowerDisp=document.getElementById("lowerRow");
 //Rows test
-/*
+
 const upperDisp=document.getElementById("upperRow");
 const lowerDisp=document.getElementById("lowerRow");
-*/
+
 const resetButton=document.getElementById("reset");
 resetButton.addEventListener('click', resetCalcTotal);
 
 const clearDisplay=document.getElementById("clearDisp");
 clearDisplay.addEventListener('click', ()=>{
-    displayField.textContent='';
+    //lowerDisp.textContent='';
+    resetDisplays();
     if(periodCheck===true) periodCheck=false;
 });
 
@@ -37,10 +38,10 @@ let math=[];
 let terminator=false, resultCheck=false, operatorCheck=false, periodCheck=false;
 
 function bksp(){    //Backspace
-    let oldDisplay=displayField.textContent;
-    if(oldDisplay.charAt(0)==='C') displayField.textContent='';
-    else if(oldDisplay.length>1) displayField.textContent=oldDisplay.slice(0, oldDisplay.length-1);
-    else if(oldDisplay.length===1) displayField.textContent=''
+    let oldDisplay=lowerDisp.textContent;
+    if(oldDisplay.charAt(0)==='C') lowerDisp.textContent='';
+    else if(oldDisplay.length>1) lowerDisp.textContent=oldDisplay.slice(0, oldDisplay.length-1);
+    else if(oldDisplay.length===1) lowerDisp.textContent=''
 }
 
 function displayNumber(num){  //Displays the number that is clicked
@@ -50,46 +51,49 @@ function displayNumber(num){  //Displays the number that is clicked
     }
 
     if(resultCheck===true){
-        displayField.textContent='';
+        lowerDisp.textContent='';
         resultCheck=false;
     }
     if(operatorCheck===true) operatorCheck=false;
 
     if(num==="period"){ //Correctly displays the dot and disables it after the first insertion in display
         if(periodCheck===false){
-            displayField.textContent+=".";
+            lowerDisp.textContent+=".";
             periodCheck=true;
         }
         return;
     }
-    displayField.textContent+=num;
+    lowerDisp.textContent+=num;
 }
 
 function takeInput(operatorId){  //When an operator is clicked, it takes the number and the operator
     if(periodCheck===true) periodCheck=false;
     if(terminator===true){     //Allows user to continue operating with the result, cleaning all the rest...
-        let swapDisp=displayField.textContent;
-        if(swapDisp.charAt(0)==='C'){   //...If a valid result is displayed
+        let lowerSwap=lowerDisp.textContent;
+        if(lowerSwap.charAt(0)==='C'){   //...If a valid result is displayed
             resetCalcTotal();
             return;
         }
+        let upperSwap=upperDisp.textContent;
+        console.log("upper swap: "+upperSwap); //logic ok
         resetCalc();
         terminator=false;
-        math.push(swapDisp);
+        lastOperation(upperSwap, lowerSwap, operatorId);
+        /*math.push(swapDisp);
         math.push(operatorId);
-        displayField.textContent='';
+        lowerDisp.textContent='';*/
         return;
     }
 
     if(math.length===0){    //First operand check
-        if(displayField.textContent===''){      //Empty display check, assume the value '0' if empty;
+        if(lowerDisp.textContent===''){      //Empty display check, assume the value '0' if empty;
             math.push('0');
             math.push(operatorId);
             return;
         }
         pushElement(operatorId);
         operatorCheck=true;
-        displayField.textContent='';
+        lowerDisp.textContent='';
         return;
     }
 
@@ -100,10 +104,10 @@ function takeInput(operatorId){  //When an operator is clicked, it takes the num
 
     pushElement(operatorId);
     operatorCheck=true;
-    displayField.textContent='';
+    lowerDisp.textContent='';
     let switchId=math.length;
 
-    if(math[switchId-1]==='percentage'){
+    if(math[switchId-1]==='percentagte'){
         let percentageValue=(parseFloat(math[switchId-4])/100)*parseFloat(math[switchId-2]);
 //        console.log("Percentage value: "+percentageValue);
         math[switchId-2]=percentageValue;
@@ -119,32 +123,78 @@ function takeInput(operatorId){  //When an operator is clicked, it takes the num
 function computeOperation(switchId){    //Actually does the math required other than the percentage calculation
     switch(math[switchId-3]){
         case 'add':
-            displayField.textContent=parseFloat(math[switchId-4])+parseFloat(math[switchId-2]);
-            math[switchId-2]=displayField.textContent;
+            lowerDisp.textContent=parseFloat(math[switchId-4])+parseFloat(math[switchId-2]);
+            math[switchId-2]=lowerDisp.textContent;
             break;
         case 'subtract':
-            displayField.textContent=parseFloat(math[switchId-4])-parseFloat(math[switchId-2]);
-            math[switchId-2]=displayField.textContent;
+            lowerDisp.textContent=parseFloat(math[switchId-4])-parseFloat(math[switchId-2]);
+            math[switchId-2]=lowerDisp.textContent;
             break;
         case 'multiply':
-            displayField.textContent=parseFloat(math[switchId-4])*parseFloat(math[switchId-2]);
-            math[switchId-2]=displayField.textContent;
+            lowerDisp.textContent=parseFloat(math[switchId-4])*parseFloat(math[switchId-2]);
+            math[switchId-2]=lowerDisp.textContent;
             break;
         case 'divide':
             if(math[switchId-2]==0){
-                displayField.textContent="Can't divide by 0!";
+                lowerDisp.textContent="Can't divide by 0!";
                 terminator=true;
                 break;
             }
-            displayField.textContent=parseFloat(math[switchId-4])/parseFloat(math[switchId-2]);
-            math[switchId-2]=displayField.textContent;
+            lowerDisp.textContent=parseFloat(math[switchId-4])/parseFloat(math[switchId-2]);
+            math[switchId-2]=lowerDisp.textContent;
             break;
     }
 }
 
 function pushElement(operatorId){   //Puts number and operator in the operands array
-    math.push(displayField.textContent);
+    math.push(lowerDisp.textContent);
     math.push(operatorId);
+    //Populate upper display
+    let operator=operatorSymbol(operatorId);
+    /*switch(operatorId){     //create separate function
+        case "add":
+            operatorSymbol="+";
+            break;
+        case "subtract":
+            operatorSymbol="-";
+            break;
+        case "multiply":
+            operatorSymbol="*";
+            break;
+        case "divide":
+            operatorSymbol="/";
+            break;
+        case "percentage":
+            operatorSymbol="%";
+            break;
+        case "operate":
+            operatorSymbol="=";
+            break;
+    }*/
+    upperDisp.textContent+=lowerDisp.textContent+operator;
+}
+
+function operatorSymbol(operatorId){
+    switch(operatorId){
+        case "add":
+            return "+";
+        case "subtract":
+            return "-";
+        case "multiply":
+            return "*";
+        case "divide":
+            return "/";
+        case "percentage":
+            return "%";
+        case "operate":
+            return "=";
+    }
+
+}
+
+function resetDisplays(){
+    upperDisp.textContent='';
+    lowerDisp.textContent='';
 }
 
 function resetCalcTotal(){   //Completely resets the calculator
@@ -156,9 +206,18 @@ function resetCalcTotal(){   //Completely resets the calculator
 }
 
 function resetCalc(){   //Resets only the operations history
-    displayField.textContent='';
+    //lowerDisp.textContent='';
+    /*upperDisp.textContent='';
+    lowerDisp.textContent='';*/
+    resetDisplays();
     math=[];
-    return;
+    //return;
+}
+
+function lastOperation(upperSwap, lowerSwap, operatorId){ //todo
+    math.push(lowerSwap);
+    math.push(operatorId);
+    upperDisp.textContent+=upperSwap+lowerSwap+operatorSymbol(operatorId);
 }
 
 function keyDisplay(e){     //Takes eventual input from the keyboard
